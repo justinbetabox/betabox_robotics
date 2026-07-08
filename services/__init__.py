e19612711b2fa3078118b8b183d2192bc408b658
Justin Leonard <justin@betabox.com>
2026-07-08 09:16:40 -0400

    fix: fixed warning in install-check

--- services/__init__.py
+++ services/__init__.py
@@ -2,12 +2,4 @@
 Betabox platform services.
 """
 
-from .install_check import main as install_check_main
-from .verify import CheckResult, collect_checks, main as verify_main
-
-__all__ = [
-    "CheckResult",
-    "collect_checks",
-    "install_check_main",
-    "verify_main",
-]
+__all__ = []
